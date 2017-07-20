fb6fa75fc48710dad6f8c3f1039c6028bed97979
Jensen Gao <921988123@qq.com>
2017-07-20 21:42:22 +0800

    Add files via upload

---
+++ TrainCaffeNetModel/Model/draw.bat
@@ -0,0 +1,2 @@
+python E:\[MyProject]\RoadClassification\PythonCaffeImplement\realtimeDrawLossAccuracy\caffe_plot.py E:\[MyProject]\RoadClassification\TrainCaffeNetModel\Model\log.txt
+pause
---
+++ TrainCaffeNetModel/Model/train.bat
@@ -0,0 +1,2 @@
+G:\caffe-master\Build\x64\Release\caffe.exe train -solver=E:\1\TrainModel\Model\solver.prototxt >log.txt 2>&1
+pause
